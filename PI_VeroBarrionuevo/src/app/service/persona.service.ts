3d9b8f01ac7a7d2574f950651e525783f553db8d
Vero Barrionuevo <109123684+VeroBarrionuevo@users.noreply.github.com>
2023-03-09 13:51:29 -0300

    Add files via upload

--- PI_VeroBarrionuevo/src/app/service/persona.service.ts
+++ PI_VeroBarrionuevo/src/app/service/persona.service.ts
@@ -1,19 +1,37 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { persona } from '../model/persona.model';
 import { environment } from 'src/environments/environment';
+import { persona } from '../model/persona.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PersonaService {
-  URL = environment.URL + 'personas/' ;
+  URL = environment.URL + 'personas/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL+'traer/perfil');
-
+  public lista(): Observable<persona[]>{
+    return this.httpClient.get<persona[]>(this.URL + "lista");
   }
+
+  public detail(id: number): Observable<persona>{
+    return this.httpClient.get<persona>(this.URL + `detail/${id}`);
+  }
+
+  // public save(experiencia: Experiencia): Observable<any>{
+  //   return this.httpClient.post<any>(this.URL + "create", experiencia);
+  // }
+
+  public update(id:number, Persona: persona): Observable<any>{
+  return this.httpClient.put<any>(this.URL + `update/${id}`, Persona);
+}
+
+//   public delete(id:number):Observable<any>{
+//     return this.httpClient.delete<any>(this.URL + `delete/${id}`);
+// }
+
 }
