b1026ec54aca7737840900ff761856c0a7c0aa60
Vero Barrionuevo <109123684+VeroBarrionuevo@users.noreply.github.com>
2023-03-09 13:55:41 -0300

    Add files via upload

--- PI_VeroBarrionuevo/src/environments/environment.prod.ts
+++ PI_VeroBarrionuevo/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'vybfrontend',
+    appId: '1:801270280699:web:76c6ca6093ef4e75115b5e',
+    storageBucket: 'vybfrontend.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDVTgLII0psIPieW9nbkloX9ZIasBZ1bu0',
+    authDomain: 'vybfrontend.firebaseapp.com',
+    messagingSenderId: '801270280699',
+    measurementId: 'G-GCQY9N90CJ',
+  },
+  production: true,
+
+  URL: 'https://portfolio-backend-production-3960.up.railway.app/' 
+
 };
--- PI_VeroBarrionuevo/src/environments/environment.ts
+++ PI_VeroBarrionuevo/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'vybfrontend',
+    appId: '1:801270280699:web:76c6ca6093ef4e75115b5e',
+    storageBucket: 'vybfrontend.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDVTgLII0psIPieW9nbkloX9ZIasBZ1bu0',
+    authDomain: 'vybfrontend.firebaseapp.com',
+    messagingSenderId: '801270280699',
+    measurementId: 'G-GCQY9N90CJ',
+  },
   production: false,
 
   URL: 'http://localhost:8090/'
